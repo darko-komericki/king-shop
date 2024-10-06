a06363aff0ad9de8f456e0f736b86bb6483f77f0
Darko Komerički <darkomericki@gmail.com>
2024-10-06 18:59:37 +0200

    close modal on esc

--- src/components/ProductModal/ProductModal.jsx
+++ src/components/ProductModal/ProductModal.jsx
@@ -1,11 +1,33 @@
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 import priceFormatter from "../../helpers/priceFormatter";
 import styles from "./ProductModal.module.scss";
 
 export default function ProductModal(props) {
-  const { showDetails, setShowDetails, product, setSelectedProduct, addToCart } = props;
+  const {
+    showDetails,
+    setShowDetails,
+    product,
+    setSelectedProduct,
+    addToCart,
+  } = props;
 
   const dialogRef = useRef(null);
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Escape") {
+      setShowDetails(false);
+      dialogRef.current.close();
+      setSelectedProduct(null);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
 
   return (
     <dialog className={styles.modal} ref={dialogRef} open={showDetails}>
@@ -13,22 +35,14 @@ export default function ProductModal(props) {
         <header className={styles.header}>
           <button
             aria-label="Close"
-            className={styles.toggle}
+            className={styles.close}
             onClick={() => {
               setShowDetails(false);
               dialogRef.current.close();
               setSelectedProduct(null);
             }}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              width="24"
-              height="24"
-            >
-              <path fill="none" d="M0 0h24v24H0z" />
-              <path d="M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636z" />
-            </svg>
+            <i class="ri-close-large-line"></i>
           </button>
         </header>
         <section className={styles.modalContent}>
